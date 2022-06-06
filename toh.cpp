db3059a26ae16c12316722401e8bf312717cc2de
Bishnu Thakur <bishnuthakur284@gmail.com>
2022-06-07 02:18:17 +0530

    Adding tower of hanoi implementaion in cpp

---
+++ toh.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void toh(int n,int a,int b,int c)
+{
+	if(n>0)
+	{
+		toh(n-1,a,c,b);
+		cout<<a<<"-"<<c<<endl;
+		toh(n-1,b,a,c);
+	}
+}
+int main(int argc,char *argv[])
+{
+	int n;
+	if(argc==2)
+		n=stoi(argv[1]);
+	else
+	{
+		cout<<"Enter number of disks= ";
+		cin>>n;
+	}
+		toh(n,1,2,3);
+	return 0;
+}
+
